28a12aae0e52c05b8a7800ef37b1c7ff00d884d3
NmiDev <missiaen.nicolas@gmail.com>
2018-08-02 19:58:12 +0200

    Init bootstrap modele for React development

---
+++ src/components/App/index.js
@@ -0,0 +1,26 @@
+/**
+ * Import
+ */
+import React from 'react';
+
+/**
+ * Local import
+ */
+// Composants
+
+// Styles et assets
+import './app.sass';
+
+/**
+ * Code
+ */
+const App = () => (
+  <div id="app">
+    <h1 id="app-title">Undefined</h1>
+  </div>
+);
+
+/**
+ * Export
+ */
+export default App;
