51419bb141a8c4edadff7bedf5e00ff9aadbde4b
NmiDev <missiaen.nicolas@gmail.com>
2018-08-09 20:53:58 +0200

    Update CSS for Message component

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,7 +2,6 @@
  * Import
  */
 import React from 'react';
-import { FaComment } from 'react-icons/fa';
 
 /**
  * Local import
@@ -12,21 +11,21 @@ import { FaComment } from 'react-icons/fa';
 
 // Styles et assets
 import './message.sass';
+import avatar from 'src/assets/images/avatar.jpg';
 
 /**
  * Code
  */
 const Message = () => (
   <div className="message">
-    <div className="message_image"><img src="src/assets/images/avatar.jpg" alt="profile" /></div>
+    <a href="#" className="message_image"><img src={avatar} alt="avatar" /></a>
     <div className="message_container">
       <div className="message_content">
         <span className="message_author">BenDevil</span>
         <span className="message_date">08.08.2018 - </span>
-        <span className="message_count">25 commentaires</span>
+        <a href="#" className="message_count">25 commentaires</a>
         <p className="message_abstract">Mon message pour tous les loulous de la promotion Invaders (j'aime pas les annonces !)</p>
       </div>
-      <div className="message_icon"><FaComment /></div>
     </div>
   </div>
 );
