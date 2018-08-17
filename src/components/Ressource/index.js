ea7eea0504a6bfb27cac43e74dcc933976bf87a5
NmiDev <missiaen.nicolas@gmail.com>
2018-08-17 16:12:42 +0200

    Ressource component global CSS is done

--- src/components/Ressource/index.js
+++ src/components/Ressource/index.js
@@ -23,7 +23,7 @@ import './ressource.sass';
 const Ressource = () => (
   <div id="ressource">
     {/* Ressource title */}
-    <h1 id="ressource_title">Un bookmark</h1>
+    <h1 id="ressource_title">Le titre du Bookmark</h1>
 
     {/* Ressource infos */}
     <p id="ressource_infos">
@@ -34,20 +34,44 @@ const Ressource = () => (
 
     {/* Ressource presentation */}
     <p id="ressource_presentation">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia perferendis quidem, porro quaerat beatae cumque asperiores totam fuga atque quisquam ipsum facere error voluptatibus nihil iste nobis, ex ut, itaque temporibus at sequi. Culpa corrupti pariatur architecto officiis repellendus aut, facere quam magni, maxime adipisci soluta? Pariatur sint blanditiis fuga animi voluptatem at voluptate quisquam quod, eveniet sed illum sunt nam sequi, eaque, quo accusamus consequuntur eos dolorum! Tenetur aspernatur consectetur tempora aliquid minima fuga ad rerum quaerat harum, unde odio sed expedita ab autem itaque est doloremque optio, quod omnis! Exercitationem, et? Maiores explicabo obcaecati quaerat vel est vero praesentium natus ex minus! Atque maiores repudiandae eveniet. Quam in sit maiores. Quos dolor beatae, quas maxime non delectus. Iste deleniti maxime repellendus labore? Adipisci inventore ab voluptatem repellendus pariatur omnis excepturi sequi iste minima quia minus facere distinctio error.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia perferendis quidem, porro quaerat beatae cumque asperiores totam fuga atque quisquam ipsum facere error voluptatibus nihil iste nobis, ex ut, itaque temporibus at sequi. Culpa corrupti pariatur architecto officiis repellendus aut, facere quam magni, maxime adipisci soluta? Pariatur sint blanditiis fuga animi voluptatem at voluptate quisquam quod, eveniet sed illum sunt nam sequi, eaque, quo accusamus consequuntur eos dolorum! Tenetur aspernatur consectetur tempora aliquid minima fuga ad rerum quaerat harum, unde odio sed expedita ab autem itaque est doloremque optio.
     </p>
 
     {/* Ressources tags */}
     <div id="ressource_tags">
-      <div className="ressource_tags_ligne">
-        <div className="ressource_tag">Tag1</div>
-        <div className="ressource_tag">Tag2</div>
-        <div className="ressource_tag">Tag3</div>
+      <div className="ressource_tags_line">
+        <div className="ressource_tags_tag">
+          <div className="tag_left" />
+          <div className="tag_content">Audio</div>
+          <div className="tag_right" />
+        </div>
+        <div className="ressource_tags_tag">
+          <div className="tag_left" />
+          <div className="tag_content">Français</div>
+          <div className="tag_right" />
+        </div>
+        <div className="ressource_tags_tag">
+          <div className="tag_left" />
+          <div className="tag_content">Progresser</div>
+          <div className="tag_right" />
+        </div>
       </div>
-      <div className="ressource_tags_ligne">
-        <div className="ressource_tag">Tag4</div>
-        <div className="ressource_tag">Tag5</div>
-        <div className="ressource_tag">Tag6</div>
+      <div className="ressource_tags_line">
+        <div className="ressource_tags_tag">
+          <div className="tag_left" />
+          <div className="tag_content">Bootstrap</div>
+          <div className="tag_right" />
+        </div>
+        <div className="ressource_tags_tag">
+          <div className="tag_left" />
+          <div className="tag_content">Javascript</div>
+          <div className="tag_right" />
+        </div>
+        <div className="ressource_tags_tag">
+          <div className="tag_left" />
+          <div className="tag_content">Librairie de ouf</div>
+          <div className="tag_right" />
+        </div>
       </div>
     </div>
 
@@ -56,21 +80,21 @@ const Ressource = () => (
     {/* Ressource links and preview */}
     <div id="ressource_preview">
       <div id="ressource_preview_section">
-        <p id="ressource_preview_title">Filtrer les bookmarks</p>
-        <div id="ressource_preview_icon"><FaFilm /></div>
+        <p id="ressource_preview_title">Previsualisation</p>
+        <div><FaFilm id="ressource_preview_icon" /></div>
       </div>
 
       <div id="ressource_link">
-        <p>Lien vers le bookmark</p>
+        <p>Lien vers le bookmark :</p>
         <a href="#">URL du bookmark</a>
       </div>
-      <div id="ressource_preview">
+      <div id="ressource_capture">
         Je suis la preview du bookmark
       </div>
     </div>
 
     {/* Return */}
-    <button type="button" id="ressource_return">Retour</button>
+    <button type="button" className="search_button">Retour</button>
 
   </div>
 );
